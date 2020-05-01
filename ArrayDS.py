6f6fd0f8fc9c719186c5111252c1e61c1c9f9e15
Khaled Md Tuhidul Hossain <mth.tuhin@gmail.com>
2020-05-01 14:42:27 +0600

    DataStructure : Array Updated
    :100:

--- ArrayDS.py
+++ ArrayDS.py
@@ -1,6 +1,19 @@
 # importing "array" for array operations
 import array
 
-arr = array.array('i', [1, 2, 3])  # initializing array
+# initializing array
+arr = array.array('i', [1, 2, 3])  
 
-print("Array Length " + arr.len)
+# printing original array 
+for x in range((len(arr)-len(arr)),len(arr)):
+	print(arr[x])
+
+# appending an item in array
+arr.append(4)
+
+#inserting multiple item in array
+arr.insert(5,6)
+
+# printing new extended array 
+for x in range((len(arr)-len(arr)),len(arr)):
+	print(arr[x])
